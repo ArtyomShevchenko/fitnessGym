777e1501f68b9b2211f39e0d523ffe7492a5f757
Admin <artyomshevchenkowarmgray@gmail.com>
2022-06-02 17:26:10 +0300

    add footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log(1)
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 180) {
+        document.querySelector(".scroll__top").style.display = "block";
+    } else {
+        document.querySelector(".scroll__top").style.display = "none";
+    }
+})
+
+document.body.addEventListener("click", (event) => {
+
+    if (event.target === document.querySelector(".burger")) {
+        document.querySelector(".burger").classList.toggle("burger_active");
+        document.querySelector(".menu__content").classList.toggle("menu__content_active");
+    };
+
+    if (event.target === document.querySelector(".scroll__top")) {
+        window.scrollTo({
+            top : 0,
+            behavior : "smooth",
+        })
+    }
+
+});
